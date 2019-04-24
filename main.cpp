723e14eb630c8dc9bff63a2dc2d8ddab9581869a
Kyle M. Tarplee <kmtarplee@anderson.edu>
2019-04-24 14:48:24 -0400

    the brake changed mechanically so we are moving the travel down on the brake control loop

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ Watchdog dog;
 iBUS ibus;
 
 // Tuning parameters
-const float ebrake_full = 3.0f;
+const float ebrake_full = 0.5f;
 
 const float steering_full = 0.15f;
 const float steering_center = 0.53f;
